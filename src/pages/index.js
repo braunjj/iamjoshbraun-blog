181ff4bc03f4c06a9d24024534ab56807ed01692
Josh Braun <josh.braun.4@gmail.com>
2021-01-24 23:42:49 -0500

    fix homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Layout from "../components/layout"
-import CaseStudies from "../components/work_case_studies"
 
 import SEO from "../components/seo"
 
@@ -13,8 +12,6 @@ export default class Home extends Component {
 
         <h1>Hello, I'm Josh</h1>
         <p>Welcome to my site</p>
-
-        <CaseStudies/>
       </Layout>
       )
     }
