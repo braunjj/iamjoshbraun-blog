0580d4655b97c77ef3e644bf10421aa471c6a18b
Josh Braun <josh.braun.4@gmail.com>
2021-01-26 19:09:22 -0500

    Fix favicon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/static/img/favicon.jpg`,
+        icon: `../src/static/img/favicon.jpg`,
       },
     },
     {
